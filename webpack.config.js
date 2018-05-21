85e86a855410f2ab742dc5fac222914fb6282be0
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-05-21 22:51:10 +0100

    logo added

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,12 @@ module.exports = {
                 template: 'src/index.html'
             }
         ),
-        // new CopyWebpackPlugin(
-        //     [
-        //         {from: 'src/assets', to: 'assets'},
-        //         {from: 'src/netlify'}
-        //     ]
-        // )
+        new CopyWebpackPlugin(
+            [
+                {from: 'src/assets', to: 'assets'},
+                {from: 'src/netlify'}
+            ]
+        )
 
     ],
     devServer: {
